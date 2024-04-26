d4da54d7e69f2bd6c4db935e58a428972736730e
pedrozo1a0 <ale.pedrozo.952@gmail.com>
2024-04-26 11:26:27 -0400

    agregando eventos en javascript

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
-/*
-- Al tocar el boton de la patita, que aparezca un alerta que diga guau guau
-- Cuando el usuario haga click en el boton adoptar, se adopte inmediatamente y desaparezca la ficha.
-*/
 
-// Variable Global
-let contadorAdoptados = 0;
-
-// Al tocar la imagen, desplega un alerta.
-function ladrar() {
-    alert("Guau guau!"); // Manda un alerta
-};
-
-// Al tocar una ficha, elimina la ficha y suma al contador.
-function adoptar(elemento) {
-    elemento.remove(); 
-    //elemento.style.display = "none"; 
-    let contadorHTML = document.querySelector("#contadorAnimales");
-    contadorAdoptados++;
-    contadorHTML.textContent = contadorAdoptados;
+window.onload= function(){
+    // Variable Global
+    let contadorAdoptados = 0;
+    
+    // Al tocar la imagen, desplega un alerta.
+    function ladrar() {
+        alert("Guau guau!"); // Manda un alerta
+    };
+    
+    // Al tocar una ficha, elimina la ficha y suma al contador.
+    function adoptar(elemento) {
+        elemento.remove(); 
+        //elemento.style.display = "none"; 
+        let contadorHTML = document.querySelector("#contadorAnimales");
+        contadorAdoptados++;
+        contadorHTML.textContent = contadorAdoptados;
+    }
+    
+    /*
+    let logoImagen = document.querySelector(".logo");
+    
+    logoImagen.addEventListener("click", function (elemento) {
+        elemento.remove();
+        //elemento.style.display = "none"; 
+        contadorAdoptados++;
+        let contadorHTML = document.querySelector("#contadorAnimales");
+        contadorHTML.textContent = contadorAdoptados;
+    })
+    */
+   let iconosPatitas= document.querySelector(".logo");
+   console.log(iconosPatitas);
 }
-
-/*
-let logoImagen = document.querySelector(".logo");
-
-logoImagen.addEventListener("click", function (elemento) {
-    elemento.remove();
-    //elemento.style.display = "none"; 
-    contadorAdoptados++;
-    let contadorHTML = document.querySelector("#contadorAnimales");
-    contadorHTML.textContent = contadorAdoptados;
-})
- */

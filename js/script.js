72c9c6d866c47d8571a74f15676c4f614ec09697
Mitsuhiro32 <mitsueiwa333@gmail.com>
2024-05-06 08:50:48 -0400

    Agregar todos los animales al script.js

--- js/script.js
+++ js/script.js
@@ -53,8 +53,30 @@ class Animal {
   }
 }
 
-//Terminar de agregar todos los animales
+// Animales
 let Miguel = new Animal(1, "Gato", "Miguel", ["Simpático", "Amigable"], false, "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg");
+
+let Pepe = new Animal(2, "Gato", "Pepe", ["Simpático", "Amigable"], false, "https://nupec.com/wp-content/uploads/2021/02/Captura-de-pantalla-2021-02-08-a-las-13.59.48.png");
+
+let Patán = new Animal(3, "Perro", "Patán", ["Simpático", "Amigable"], false, "https://definicion.de/wp-content/uploads/2013/03/perro-1.jpg");
+
+let Huesos = new Animal(4, "Perro", "Huesos", ["Simpático", "Amigable"], false, "https://i.ibb.co/sWw7f0Y/perro-huesos.png");
+
+let Akira = new Animal(5, "Perra", "Akira", ["Simpática", "Amigable"], false, "https://pampermut.com/blog/wp-content/uploads/2020/05/Como-es-el-caracter-de-tu-perro-segun-su-horoscopo-scaled.jpg");
+
+let Laika = new Animal(6, "Perra", "Laika", ["Simpática", "Amigable"], false, "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg");
+
+let Pluto = new Animal(7, "Lemur", "Pluto", ["Simpático", "Amigable"], false, "https://www.akronzoo.org/sites/default/files/styles/uncropped_xl/public/2022-05/Ring-tailed-lemur-main.png?itok=T7g7bQt6");
+
+let Marley = new Animal(8, "Ratón", "Marley", ["Simpático", "Amigable"], false, "https://www.lavanguardia.com/files/og_thumbnail/uploads/2019/05/09/5fa5334e4f8da.jpeg");
+
+let Paco = new Animal(9, "Ñandú", "Paco", ["Simpático", "Amigable"], false, "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/183424031/1800");
+
+let Pinky = new Animal(10, "Ratón", "Pinky", ["Simpático", "Amigable"], false, "https://img.freepik.com/foto-gratis/primer-plano-sobre-rata-blanca_23-2150760628.jpg");
+
+let NieblaDiabla = new Animal(11, "Hamster", "Niebla Diabla", ["Simpático", "Amigable"], false, "https://www.zooplus.es/magazine/wp-content/uploads/2019/04/hamster-rasseportrait.jpg");
+
+let Anónima = new Animal(12, "Serpiente", "Anónima", ["Simpática", "Amigable"], false, "https://d2r8r0qhs4bt8m.cloudfront.net/wp-content/uploads/2019/10/18163743/desktop-1425399420.jpg");
 
 function crearAnimal(unAnimal) {  //Falta imagen y adoptado
   let nuevaFicha = document.createElement("div");
